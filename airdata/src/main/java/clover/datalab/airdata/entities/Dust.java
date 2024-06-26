7192259780edd7658c252923864814a6ffce4cf8
mbc <dksdudwls888@naver.com>
2024-06-26 10:25:46 +0900

    db

--- airdata/src/main/java/clover/datalab/airdata/entities/Dust.java
+++ airdata/src/main/java/clover/datalab/airdata/entities/Dust.java
@@ -18,30 +18,34 @@ import java.time.LocalDateTime;
 @Table(name = "dusts")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Dust {
+	
+    public String getDataTime() {
+        return this.dataTime;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 고유번호(ID, 자동생성)
 
-    @Column(nullable = false, name = "sido_name")
+    @Column(nullable = true, name = "sido_name")
     private String sidoName; // 시도명
 
-    @Column(nullable = false, name = "data_time")
+    @Column(nullable = true, name = "data_time")
     private String dataTime; // 측정일시
 
-    @Column(nullable = false, name = "station_name")
+    @Column(nullable = true, name = "station_name")
     private String stationName; // 측정소명
 
-    @Column(nullable = false, name = "station_code")
+    @Column(nullable = true, name = "station_code")
     private String stationCode; // 측정소 코드
 
-    @Column(nullable = false, name = "mang_name")
+    @Column(nullable = true, name = "mang_name")
     private String mangName; // 측정망 정보
 
-    @Column(nullable = false, name = "pm10_value")
+    @Column(nullable = true, name = "pm10_value")
     private String pm10Value; // 미세먼지 농도
 
-    @Column(nullable = false, name = "pm25_value")
+    @Column(nullable = true, name = "pm25_value")
     private String pm25Value; // 초미세먼지 농도
     
     @Column(nullable = true, name = "pm10_grade")
