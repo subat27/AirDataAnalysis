b92c151d34bcf7d5ece6523aa36faaff8813b834
Leesuyeon <lsy940602@gmail.com>
2024-06-27 10:22:52 +0900

    상품관리 개발 (관리자파트)
    * 라이프스타일과의 연동을 위해 해당 파트의 개발이 완료된 후 소스 수정 필요.

---
+++ airdata/src/main/java/clover/datalab/airdata/services/IProductService.java
@@ -0,0 +1,81 @@
+package clover.datalab.airdata.services;
+
+import clover.datalab.airdata.entities.Lifestyle;
+import clover.datalab.airdata.entities.Product;
+import clover.datalab.airdata.http.forms.ProductForm;
+import clover.datalab.airdata.repositories.ProductRepository;
+import clover.datalab.airdata.utilities.Common;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class IProductService implements ProductService {
+
+	private final ProductRepository repository;
+	
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void register(ProductForm form, Lifestyle lifestyle) {
+    	repository.save(
+    		Product.builder().subject(form.getSubject())
+			    			 .content(form.getContent())
+			    			 .price(form.getPrice())
+			    			 .tags(form.getTags())
+			    			 .thumbnail(form.getThumbnail())
+			    			 .category(form.getCategory())
+			    			 .link(form.getLink())
+			    			 .lifestyle(lifestyle)
+			    			 .build()
+    	);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void update(ProductForm form, Lifestyle lifestyle, Long id) throws Exception {
+    	Product product = currentItem(id);
+    	
+    	product.setSubject(form.getSubject());
+    	product.setContent(form.getContent());
+    	product.setCategory(form.getCategory());
+    	product.setThumbnail(form.getThumbnail());
+    	product.setPrice(form.getPrice());
+    	product.setTags(form.getTags()); 
+    	product.setLink(form.getLink());
+    
+    	repository.save(product);
+    }
+
+    @Override
+    public void delete(Long id) {
+    	if (repository.existsById(id)) {
+    		repository.deleteById(id);    	
+    	}
+    }
+
+    @Override
+    public Product currentItem(Long id) throws Exception {
+        return repository.findById(id).orElseThrow(() -> new Exception("데이터가 없습니다."));
+    }
+
+    @Override
+    public Map<String, Object> paginatedItem(int page, int perPage, String searchType, String searchWord) {
+    	Pageable pageable = PageRequest.of((page - 1), perPage);
+    	
+    	Page<Product> items = switch(searchType) {
+    		case "subject" -> repository.findAllBySubjectContainsOrderByIdDesc(searchWord, pageable);
+    		case "content" -> repository.findAllByContentContainsOrderByIdDesc(searchWord, pageable);
+    		default -> repository.findAllByOrderByIdDesc(pageable);
+    	};
+    	
+        return Common.paginate(page, items, searchWord, searchType, "");
+    }
+
+}
---
+++ airdata/src/main/java/clover/datalab/airdata/services/ProductService.java
@@ -0,0 +1,19 @@
+package clover.datalab.airdata.services;
+
+import clover.datalab.airdata.entities.Lifestyle;
+import clover.datalab.airdata.entities.Product;
+import clover.datalab.airdata.http.forms.ProductForm;
+
+import java.util.Map;
+
+public interface ProductService {
+
+    void register(ProductForm form, Lifestyle lifestyle);
+    void update(ProductForm form, Lifestyle lifestyle, Long id) throws Exception;
+    void delete(Long id);
+
+    Product currentItem(Long id) throws Exception;
+
+    Map<String, Object> paginatedItem(int page, int perPage, String searchType, String searchWord);
+
+}
